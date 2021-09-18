473ae17ae25dc54afc21e750216e06dcb9f73383
Maria-Tkh <mcweaver117@gmail.com>
2021-09-18 10:22:50 +0300

    loader added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import { Component } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 import { fetchImages } from './api';
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 import { Button } from './Button/Button';
 import { Modal } from './Modal/Modal';
+import { Load } from './Loader/Loader';
+
 // import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 
 export class App extends Component {
@@ -11,7 +15,7 @@ export class App extends Component {
     imageTags: '',
     page: 1,
     gallery: [],
-    requestState: 'idle',
+    requestStatus: 'idle',
     showModal: false,
     largeImageURL: '',
   };
@@ -44,9 +48,20 @@ export class App extends Component {
     const { imageTags, page } = this.state;
     if (prevState.imageTags !== imageTags || prevState.page !== page)
       try {
+        this.setState({ requestStatus: 'pending' });
         const gallery = await fetchImages(imageTags, page);
-        this.setState({ gallery: [...prevState.gallery, ...gallery] });
+        this.setState({
+          gallery: [...prevState.gallery, ...gallery],
+          requestStatus: 'resolved',
+        });
+        if (gallery.length === 0) {
+          return toast('Sorry, there are no images matching your search query. Please try again.');
+        }
+        this.setState({
+          gallery: [...prevState.gallery, ...gallery],
+        });
       } catch (error) {
+        // this.setState({ requestStatus: 'rejected' });
         // console.log('error');
       }
 
@@ -57,8 +72,9 @@ export class App extends Component {
   }
 
   render() {
-    const { gallery, showModal, largeImageURL, imageTags } = this.state;
-    const showGallery = gallery.length > 0;
+    const { gallery, showModal, largeImageURL, imageTags, requestStatus } = this.state;
+    const isLoading = requestStatus === 'pending';
+    const showGallery = gallery.length > 0 && !isLoading;
 
     return (
       <div>
@@ -66,9 +82,11 @@ export class App extends Component {
           <Modal onClose={this.toggleModal} largeImageURL={largeImageURL} alt={imageTags} />
         )}
         <Searchbar onSearch={this.handleFormSubmit} />
+        {isLoading && <Load />}
         <ImageGallery gallery={gallery} handleSelectedImage={this.handleSelectedImage} />
         {showGallery && <Button handleLoadMore={this.handleLoadMore} />}
-        {/* < ImageGalleryItem handleSelectedImage={ this.toggleModal}/> */}
+        <Toaster position="top-right" />
+        <ImageGalleryItem handleSelectedImage={this.toggleModal} />
       </div>
     );
   }
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,8 @@
+import Loader from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+
+export const Load = () => {
+  //other logic
+
+  return <Loader type="Rings" color="#00BFFF" height={80} width={80} timeout={3000} />;
+};
